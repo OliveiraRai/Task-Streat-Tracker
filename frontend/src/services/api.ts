7744f742014798bd64fe1f586c92e4661366769a
rai <rai.conslog@hotmail.com>
2026-03-14 00:24:29 -0300

    fix: testando url de api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://task-streak-tracker.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    // O Django TokenAuthentication espera o prefixo "Token "
     config.headers.Authorization = `Token ${token}`;
   }
   return config;
